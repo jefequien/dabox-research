7722e9d7203d4c919a703ddf9d14061871949c3f
Jeffrey Hu <hujh14@gmail.com>
2024-03-20 11:06:16 -0700

    upload to hf

--- dabox_research/env.py
+++ dabox_research/env.py
@@ -6,3 +6,5 @@ from pathlib import Path
 ROOT_DIR = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 DEMO_DIR = ROOT_DIR / "demo"
 DEFAULT_OUTPUT_DIR = ROOT_DIR / ".output"
+
+HUGGING_FACE_MODEL_ZOO_REPO = "jefequien/dabox"
---
+++ tools/upload_yolov8.py
@@ -0,0 +1,33 @@
+"""Open a PR to upload a model to huggingface"""
+import argparse
+
+from huggingface_hub import HfApi
+from pathlib import Path
+
+from dabox_research.env import HUGGING_FACE_MODEL_ZOO_REPO
+
+
+def main(args):
+    """Open a PR to upload a model to huggingface"""
+    src_path = Path(args.src_path)
+    dst_path = Path(args.dst_path)
+
+    api = HfApi()
+    api.upload_file(
+        path_or_fileobj=str(src_path),
+        path_in_repo=str(dst_path),
+        repo_id=HUGGING_FACE_MODEL_ZOO_REPO,
+        repo_type="model",
+        commit_message=f"Uploaded {src_path}!",
+        create_pr=True,
+    )
+    print("Review PR at https://huggingface.co/jefequien/dabox/discussions")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--src-path", required=True, help="Path of file to upload")
+    parser.add_argument("-d", "--dst-path", required=True, help="Path in repo to upload")
+    input_args = parser.parse_args()
+    print(input_args)
+    main(input_args)
