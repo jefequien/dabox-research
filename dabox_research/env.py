ef08f88d0192aca6674e66eee38f3845dc0d63f1
Jeffrey Hu <hujh14@gmail.com>
2024-03-20 10:09:40 -0700

    Add preprocessing and postprocessing to Yolov8 onnx models. (#9)

--- dabox_research/env.py
+++ dabox_research/env.py
@@ -4,3 +4,5 @@ import os
 from pathlib import Path
 
 ROOT_DIR = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+DEMO_DIR = ROOT_DIR / "demo"
+DEFAULT_OUTPUT_DIR = ROOT_DIR / ".output"
---
+++ tools/export_resnet50.py
@@ -0,0 +1,27 @@
+import torch
+from torchvision import models
+
+
+def main():
+    model = models.resnet50(pretrained=True)
+    model.eval()
+
+    input_dim = (1, 3, 224, 224)
+    dummy_input = torch.randn(input_dim)
+
+    onnx_path = "resnet50.onnx"
+    dynamic = {"input": {0: "batch"}, "output": {0: "batch"}}
+    torch.onnx.export(
+        model,
+        dummy_input,
+        onnx_path,
+        verbose=True,
+        input_names=["input"],
+        output_names=["output"],
+        dynamic_axes=dynamic,
+        opset_version=17,
+    )
+
+
+if __name__ == "__main__":
+    main()
